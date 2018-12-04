3c26e4bcee176dde06bc73002c0230059707ff86
Fabiano de Oliveira Rodrigues <fabiano.1972@hotmail.com>
2018-12-04 02:14:20 -0200

    Correção

--- SmartQueue/SmartQueue/Model/Pedido.cs
+++ SmartQueue/SmartQueue/Model/Pedido.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace SmartQueue.Model
 {
     public class Pedido
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
-
+        [JsonProperty("contaId")]
         public int ContaId { get; set; }
-
+        [JsonProperty("data")]
         public DateTime Data { get; set; }
-
+        [JsonProperty("status")]
         public string Status { get; set; }
     }
 }
